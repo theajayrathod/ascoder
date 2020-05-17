46dfcbe5be31c6663764bbdebd51f2372df1e67b
Ajay Rathod <rathodajay60@gmail.com>
2020-05-17 17:39:20 +0530

    comment one part done

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    #Api to post comment
+    path('postComment', views.postComment, name= "postComment"),
     path('', views.blogHome, name='blogHome'),
     path('<str:slug>', views.blogPost, name='blogPost'),
+
     
 ]

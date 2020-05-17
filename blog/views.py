1516ab869f2d24b5aa64a1079885a7b7e092b173
Ajay Rathod <rathodajay60@gmail.com>
2020-05-17 20:51:38 +0530

    replay is almost over for comment

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from blog.models import *
+from blog.templatetags import extras
 from django.contrib import messages
 
 # Create your views here.
@@ -11,8 +12,17 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug =slug).first()
-    comments = BlogComment.objects.filter(post = post)
-    context = {'post':post, 'comments':comments, 'user':request.user}
+    comments = BlogComment.objects.filter(post = post, parent = None)
+    replies = BlogComment.objects.filter(post = post).exclude(parent = None)
+    replyDict = {}
+    for reply in replies:
+        if reply.parent.sno not in replyDict.keys():
+            replyDict[reply.parent.sno] = [reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
+
+        print(replyDict)
+    context = {'post':post, 'comments':comments, 'user':request.user, 'replyDict':replyDict}
     return render(request, "blog/blogPost.html", context)
 
 def postComment(request):
@@ -21,9 +31,16 @@ def postComment(request):
         user = request.user
         postSno = request.POST.get("postSno")
         post = Post.objects.get(sno = postSno)
+        parentSno = request.POST.get("parentSno")
 
-        comment = BlogComment(comment = comment, user = user, post = post)
-        comment.save()
-        messages.success(request, "Your comment has been successfully")
+        if parentSno == "":
+            comment = BlogComment(comment = comment, user = user, post = post)
+            comment.save()
+            messages.success(request, "Your comment has been successfully")
+        else:
+            parent = BlogComment.objects.get(sno = parentSno)
+            comment = BlogComment(comment = comment, user = user, post = post, parent = parent)
+            comment.save()
+            messages.success(request, "Your reply has been successfully")
 
     return redirect(f'/blog/{post.slug}')
